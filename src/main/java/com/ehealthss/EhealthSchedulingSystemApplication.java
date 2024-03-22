49f42a3dd791ddbf02340d6f15da6b678ec82ac3
Edgardo Guzman Jr <edgardoguzmanjr@viv-macbook-pro.gateway.sasktel.net>
2024-03-21 21:07:28 -0600

    initial commit

---
+++ src/main/java/com/ehealthss/EhealthSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ehealthss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhealthSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhealthSchedulingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehealthss/EhealthSchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehealthss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhealthSchedulingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
