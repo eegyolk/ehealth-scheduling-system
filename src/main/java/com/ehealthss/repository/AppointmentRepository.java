7d760115b3c7ad6478104fff4e2de6d6d5eed737
Edgardo Guzman Jr <edgardoguzmanjr@viv-macbook-pro.gateway.sasktel.net>
2024-03-24 17:21:58 -0600

    Update AppointmentRepository
    Removed @Query annotation, instead of creating a query, this will be created based on the
    given name of the method.

--- src/main/java/com/ehealthss/repository/AppointmentRepository.java
+++ src/main/java/com/ehealthss/repository/AppointmentRepository.java
@@ -1,12 +1,10 @@
 package com.ehealthss.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.ehealthss.model.Appointment;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 
-	@Query("select a from Appointment a where a.referenceNo = ?1")
 	Appointment findByReferenceNo(String referenceNo);
 }
